6150dba3ea6bc89f40370d85e6495bb085f0d89c
Victor C Tavernari <victortavernari@gmail.com>
2021-01-16 19:07:27 -0300

    Fix it does not handle error

--- CharacterList/Sources/CharacterList/Data/CharactersRestRepository.swift
+++ CharacterList/Sources/CharacterList/Data/CharactersRestRepository.swift
@@ -12,18 +12,20 @@ class CharactersRestRepository: CharactersRepository {
     private var allCharactersCancelable: AnyCancellable?
     func fetch(page: Int) -> Future<[CharacterList.Character], Error> {
         return Future<[CharacterList.Character], Error> { promise in
-            self.allCharactersCancelable = RickAndMortyRestAPI.allCharacters(page: page).sink { error in
-                print(error)
+            self.allCharactersCancelable = RickAndMortyRestAPI.allCharacters(page: page).sink { completion in
+                switch completion {
+                case .failure(let error):
+                    promise(.failure(error))
+                default: break
+                }
             } receiveValue: { fetchResponse in
-                promise(.success( fetchResponse
-                                    .results
-                                    .map { char in
-                                        return Character(id: char.id,
-                                                         name: char.name,
-                                                         avatar: char.image)
-                                    }
-                    )
-                )
+                let characters = fetchResponse.results.map { char in
+                        return Character(id: char.id,
+                                         name: char.name,
+                                         avatar: char.image)
+                }
+
+                promise(.success( characters ))
             }
         }
     }
